cef4a89a180f56e944f9d7062603cc4d0c1f5e62
JosTheDude <jos@joscodes.xyz>
2025-10-28 18:42:41 -0400

    feat: zip export for ps1

--- Scripts/exports.ps1
+++ Scripts/exports.ps1
@@ -1,9 +1,6 @@
 # Script: exports.ps1
 # Description: Exports each directory using packwiz and saves the exports to ./exports/
 
-# This script does not currently do CurseForge imports. This is a to-do.
-# CONTRIBUTIONS APPRECIATED FOR CURSEFORGE IMPORTS HERE
-
 $exportsDir = Join-Path (Get-Location) "exports"
 $version = "1.3.1"
 
@@ -33,9 +30,13 @@ Get-ChildItem -Directory | Where-Object { $_.Name -ne "exports" } | ForEach-Obje
 
         Push-Location $fullDirPath
 
-        # Run packwiz modrinth export
-        $outputFile = Join-Path $exportSubDir "Dev-Toolkit-$dirName-$version.mrpack"
-        & packwiz modrinth export -o $outputFile
+        # CurseForge Export
+        $curseforgeFile = Join-Path $exportSubDir "Dev-Toolkit-$dirName-$version.zip"
+        & packwiz curseforge export -o $curseforgeFile
+
+        # Modrinth Export
+        $modrinthFile = Join-Path $exportSubDir "Dev-Toolkit-$dirName-$version.mrpack"
+        & packwiz modrinth export -o $modrinthFile
 
         Pop-Location
 
